09b6b9a1c16ffa624ae071f46e493316c3de583d
Alex Work <a.l.e.k.s.93@mail.ru>
2024-03-20 00:27:07 +0500

    feat: add read from json and placing text on the page

--- Hungman/JS/word.js
+++ Hungman/JS/word.js
@@ -1,37 +1,69 @@
-let word = ["m","a","s","s","i","v","s"];
-let summ=0;
-let letter = "s";
-
-    for (let j = 0; j < word.length; j++) {
-    console.log(word[j]);
-        if (word[j]== letter) {
-        summ++;
-            console.log(word[j] +"="+ letter);
-        }
-}    
-console.log(summ);
-
 let conteiner = document.createElement('div');
+let tree = document.createElement('div');
+let conteinerTask=document.createElement('div');
+let imgTree=document.createElement('div');
+let attempts=document.createElement('div');
+let task=document.createElement('div');
+let keyboard=document.createElement('div');
+let mystery=document.createElement('div');
+let word=document.createElement('div');
+let par1=document.createElement('p');
+let par2=document.createElement('p');
+let par3=document.createElement('p');
+let Er=3;
+
 conteiner.className = "conteiner";
 document.body.prepend(conteiner);
-
-let tree = document.createElement('div');
 tree.className="tree";
-let conteinerTask=document.createElement('div');
 conteinerTask.className="conteinerTask";
 conteiner.prepend(tree,conteinerTask);
 
-let imgTree=document.createElement('div');
 imgTree.className="imgTree";
-let attempts=document.createElement('div');
 attempts.className="attempts";
+attempts.id="attempts";
 tree.prepend(imgTree,attempts);
 
-let task=document.createElement('div');
 task.className="task";
-let keyboard=document.createElement('div');
 keyboard.className="keyboard";
 conteinerTask.prepend(task,keyboard);
+
+mystery.className="mystery";
+mystery.id="mystery";
+word.className="word";
+word.id="word";
+task.prepend(word,mystery);
+
+par1.className="par1";
+par1.id="parId1";
+par2.className="par2";
+par2.id="parId2";
+par3.className="par3";
+par3.id="parId3";
+imgTree.prepend(par1,par2,par3);
+
+document.getElementById("word").innerHTML="Ответ";
+document.getElementById("mystery").innerHTML="Текст загадки";
+document.getElementById('attempts').innerHTML="Осталось попыток: "+ Er;
+document.getElementById("parId1").innerHTML="Попытка 1";
+document.getElementById("parId2").innerHTML="Попытка 2";
+document.getElementById("parId3").innerHTML="Попытка 3";
+
+async function responsed (){
+    const resp = await fetch('json/mystery.json')
+    return resp.json();
+  }
+
+async function go (){
+  
+    let respjson = await responsed();
+    console.log(respjson);
+    let mystery1 = respjson[0];
+    document.getElementById("mystery").innerHTML=mystery1.mystery;
+    document.getElementById("word").innerHTML=mystery1.answer;
+}
+
+go();
+
 
 // Задача поиска min max в массиве
 
@@ -50,3 +82,17 @@ conteinerTask.prepend(task,keyboard);
         
 // }
 // console.log("min=" + min + "  max=" + max);
+
+// let word = ["m","a","s","s","i","v","s"];
+// let summ=0;
+// let letter = "s";
+
+//     for (let j = 0; j < word.length; j++) {
+//     console.log(word[j]);
+//         if (word[j]== letter) {
+//         summ++;
+//             console.log(word[j] +"="+ letter);
+//         }
+// }    
+// console.log(summ);
+
